d831506a0b67fac8e3ddc7b61185d31d2c998acd
yuka touma <yukatouma@yukanoMacBook-Air.local>
2025-10-12 16:21:44 +0900

    Update TOC

--- src/main/resources/static/js/darkmode.js
+++ src/main/resources/static/js/darkmode.js
@@ -15,3 +15,38 @@ btn.addEventListener('click', () => {
 
 //フッターの日付
 document.getElementById("year").textContent = new Date().getFullYear();
+
+//目次
+const toc = document.querySelector(".toc-fixed");
+const toggleBtn = document.querySelector(".toc-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  toc.classList.toggle("toc-hidden");
+
+  if (toc.classList.contains("toc-hidden")) {
+    toggleBtn.textContent = "+";
+  } else {
+    toggleBtn.textContent = "−";
+  }
+});
+
+const tocLinks = document.querySelectorAll(".toc-fixed a");
+const headings = document.querySelectorAll(".article h2, .article h3");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  headings.forEach(h => {
+    const top = h.getBoundingClientRect().top;
+    if (top <= 120) {
+      current = h.id;
+    }
+  });
+
+  tocLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
